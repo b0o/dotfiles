fba2cd2ecbf017d96de54676a9c6343f7c5a7d85
Maddison Hellstrom <maddy@na.ai>
2026-01-11 08:18:35 -0800

    feat(nix): add pinentry-rofi

--- nix/package-groups/niri.nix
+++ nix/package-groups/niri.nix
@@ -10,6 +10,9 @@ with pkgs; [
   # TODO: re-enable once my PR is merged: https://github.com/b0o/wl-clipboard-rs/tree/feat-cli-copy-multi
   # wl-clipboard-rs # wl-clipboard replacement (wl-copy/wl-paste), written in Rust
 
+  # Utilities
+  pinentry-rofi # rofi frontend for pinentry
+
   # UI
   ttf-pragmasevka # Pragmata Pro doppelgänger made of Iosevka SS08
 ]
