3efd2783d726a53fda3bd3d215fb4bcbd1652fd3
Maddison Hellstrom <maddy@na.ai>
2026-01-11 05:03:28 -0800

    fix(nix): use latest nushellPlugins.skim with overlay

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -8,6 +8,31 @@
   modifications = final: prev: {
     wlr-which-key-b0o = inputs.wlr-which-key-b0o.packages.${final.stdenv.hostPlatform.system}.default;
     ghostty = inputs.ghostty.packages.${final.stdenv.hostPlatform.system}.default;
+
+    # TODO: remove once https://github.com/NixOS/nixpkgs/pull/466454 is merged
+    # Bump nu_plugin_skim to v0.21.0 for nushell 0.109.x compatibility
+    nushellPlugins =
+      prev.nushellPlugins
+      // {
+        skim = let
+          version = "0.21.0";
+          src = final.fetchFromGitHub {
+            owner = "idanarye";
+            repo = "nu_plugin_skim";
+            rev = "v${version}";
+            hash = "sha256-cFk+B2bsXTjt6tQ/IVVefkOTZKjvU1hiirN+UC6xxgI=";
+          };
+          cargoHash = "sha256-eNT4NfSlyKuVUlOrmSNoimJJ1zU88prSemplbBWcyag=";
+        in
+          prev.nushellPlugins.skim.overrideAttrs (oldAttrs: {
+            inherit version src;
+            cargoDeps = final.rustPlatform.fetchCargoVendor {
+              inherit src;
+              name = "${oldAttrs.pname}-${version}-vendor";
+              hash = cargoHash;
+            };
+          });
+      };
   };
 in
   inputs.nixpkgs.lib.composeManyExtensions [
