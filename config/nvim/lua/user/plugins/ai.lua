8ce537c9e823581923d576ca2f9a39a8112f0cbe
Maddison Hellstrom <maddy@na.ai>
2026-01-30 23:04:48 -0800

    feat(nvim): add OpencodeAsk command and commented minuet-ai config

--- config/nvim/lua/user/plugins/ai.lua
+++ config/nvim/lua/user/plugins/ai.lua
@@ -1,8 +1,11 @@
+local xk = require('user.keys').xk
+
 ---@type LazySpec[]
 return {
   {
     'supermaven-inc/supermaven-nvim',
     event = 'VeryLazy',
+    enabled = true,
     config = function()
       require('supermaven-nvim').setup {
         disable_keymaps = true,
@@ -18,7 +21,6 @@ return {
       }
 
       local c = require 'supermaven-nvim.completion_preview'
-      local xk = require('user.keys').xk
       local map = require('user.util.map').map
 
       map('i', { xk [[<C-\>]], '' }, c.on_accept_suggestion, 'SuperMaven: Accept')
@@ -43,6 +45,65 @@ return {
   {
     'NickvanDyke/opencode.nvim',
     event = 'VeryLazy',
-    config = function() vim.g.opencode_opts = {} end,
+    config = function()
+      vim.g.opencode_opts = {}
+
+      local cmd = vim.api.nvim_create_user_command
+      cmd('OpencodeAsk', function(o) require('opencode').ask(o.args, {}) end, {
+        nargs = '?',
+        -- complete = 'customlist,v:lua.require"user.util.opencode".complete',
+        desc = 'Opencode: Ask for a code snippet',
+      })
+    end,
   },
+  -- {
+  --   'milanglacier/minuet-ai.nvim',
+  --   event = 'VeryLazy',
+  --   config = function()
+  --     require('minuet').setup {
+  --       virtualtext = {
+  --         auto_trigger_ft = { 'python' },
+  --         keymap = {
+  --           -- accept whole completion
+  --           accept = xk [[<C-\>]],
+  --           -- accept one line
+  --           accept_line = xk [[<C-S-\>]],
+  --           -- accept n lines (prompts for number)
+  --           -- e.g. "A-z 2 CR" will accept 2 lines
+  --           accept_n_lines = nil,
+  --           prev = nil,
+  --           next = nil,
+  --           dismiss = nil,
+  --         },
+  --       },
+  --       provider_options = {
+  --         openai_compatible = {
+  --           api_key = function() return require('user.private').cerebras_api_key end,
+  --           -- model = 'qwen-3-235b-a22b-instruct-2507',
+  --           model = 'zai-glm-4.7',
+  --           end_point = 'https://api.cerebras.ai/v1/chat/completions',
+  --           debounce = 250,
+  --           throttle = 750,
+  --           -- system = {
+  --           --   -- prompt = function() return '/no_think\n' .. require('minuet.config').default_system.prompt end,
+  --           --   prompt = function() return require('minuet.config').default_system_prefix_first.prompt end,
+  --           -- },
+  --           system = require('minuet.config').default_system_prefix_first,
+  --           -- few_shots = function() return require('minuet.config').default_few_shots end,
+  --           -- chat_input = {},
+  --           stream = true,
+  --           name = 'cerebras',
+  --           optional = {
+  --             max_tokens = 256,
+  --             disable_reasoning = true,
+  --             -- enable_thinking = false,
+  --             -- reasoning_effort = 'low',
+  --             -- reasoning_format = 'none',
+  --           },
+  --         },
+  --       },
+  --       provider = 'openai_compatible',
+  --     }
+  --   end,
+  -- },
 }
