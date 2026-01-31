3ebee11b3491427056bc31614959a419805987ba
Maddison Hellstrom <maddy@na.ai>
2026-01-30 23:10:20 -0800

    feat(nix): add nixd, tombi, lua-language-server LSPs

--- nix/package-groups/neovim.nix
+++ nix/package-groups/neovim.nix
@@ -7,16 +7,18 @@ with pkgs; [
   # Nix
   alejandra # black-inspired formatting
   nil # language server
+  nixd # Nix language server, based on nix libraries
   statix # linter
 
   ## LSPs
   just-lsp # lsp server for just files
-  tombi # TOML Formatter / Linter / Language Server
-  vscode-langservers-extracted # vscode-{css,eslint,html,json,markdown}-langserver
   svelte-language-server # svelte language server
   tailwindcss-language-server # tailwindcss language server
+  tombi # TOML Formatter / Linter / Language Server
   typescript-go # tsgo - typescript language server (golang re-implementation)
+  vscode-langservers-extracted # vscode-{css,eslint,html,json,markdown}-langserver
   yaml-language-server # yaml language server
+  lua-language-server # lua language server
 
   ## Formatters
   dprint # multi-language formatter
