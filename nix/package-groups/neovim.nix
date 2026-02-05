a559a2f73fc68efc4a112ae5ec29e64969e62db7
Maddison Hellstrom <maddy@na.ai>
2026-02-04 18:38:24 -0800

    feat(nix): add neovim plugins and formatters
    - Add blink.cmp via flake input using nvimLib.mkLazyPlugin
    - Add topiary and topiary-nushell for tree-sitter formatting
    - Add nufmt for nushell formatting

--- nix/package-groups/neovim.nix
+++ nix/package-groups/neovim.nix
@@ -1,28 +1,42 @@
-{pkgs, ...}:
-with pkgs; [
-  neovim-nightly
-  tree-sitter-nightly
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  nvimLib = import ../lib/neovim.nix {inherit pkgs;};
+in
+  with pkgs; [
+    neovim-nightly
+    tree-sitter-nightly
 
-  ### Lanugage Servers / Tools
-  # Nix
-  alejandra # black-inspired formatting
-  nil # language server
-  nixd # Nix language server, based on nix libraries
-  nixfmt # nix formatter
-  statix # linter
+    # Nix-managed plugins (installed to ~/.nix-profile/share/nvim/lazy/)
+    (nvimLib.mkLazyPlugin "blink.cmp" inputs.blink-cmp.packages.${pkgs.system}.default)
 
-  ## LSPs
-  just-lsp # lsp server for just files
-  svelte-language-server # svelte language server
-  tailwindcss-language-server # tailwindcss language server
-  tombi # TOML Formatter / Linter / Language Server
-  typescript-go # tsgo - typescript language server (golang re-implementation)
-  vscode-langservers-extracted # vscode-{css,eslint,html,json,markdown}-langserver
-  yaml-language-server # yaml language server
-  lua-language-server # lua language server
+    ### Lanugage Servers / Tools
+    # Nix
+    alejandra # black-inspired formatting
+    nil # language server
+    nixd # Nix language server, based on nix libraries
+    nixfmt # nix formatter
+    statix # linter
 
-  ## Formatters
-  dprint # multi-language formatter
-  stylua # lua formatter
-  pruner # TreeSitter-powered formatter orchestrator
-]
+    ## LSPs
+    just-lsp # lsp server for just files
+    svelte-language-server # svelte language server
+    tailwindcss-language-server # tailwindcss language server
+    tombi # TOML Formatter / Linter / Language Server
+    typescript-go # tsgo - typescript language server (golang re-implementation)
+    vscode-langservers-extracted # vscode-{css,eslint,html,json,markdown}-langserver
+    yaml-language-server # yaml language server
+    lua-language-server # lua language server
+
+    ## Formatters
+    pruner # tree-sitter formatter orchestrator supporting injected code blocks
+    dprint # fast multi-language formatter
+    stylua # lua formatter
+    nufmt # nushell formatter (TODO: use nufmt flake)
+
+    ## Topiary
+    topiary # tree-sitter based generic formatter
+    inputs.topiary-nushell.packages.${pkgs.system}.topiary-nushell # topairy nushell formatter
+  ]
